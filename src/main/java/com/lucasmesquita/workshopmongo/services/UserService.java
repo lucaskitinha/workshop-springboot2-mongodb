c420910011de5a3069bf1aabfa12e3e2ddc5b59b
Lucas Mesquita <lucas.lmso@gmail.com>
2022-07-04 14:09:18 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/lucasmesquita/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.lucasmesquita.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasmesquita.workshopmongo.domain.User;
+import com.lucasmesquita.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
