d4134f3f7d698db56762ba2b1af0e9fbe50f96d9
Lucas Mesquita <lucas.lmso@gmail.com>
2022-07-05 19:09:49 -0300

    Consulta simples com query methods

--- src/main/java/com/lucasmesquita/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/lucasmesquita/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.lucasmesquita.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,5 @@ import com.lucasmesquita.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
-	
-
+	List<Post> findByTitleContainingIgnoreCase (String text);
 }
